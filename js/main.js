e9354790a8af3272fed99bcc95f9f5ee2914af95
Alonso Tacanga <alonso.tacanga@gmail.com>
2017-11-14 23:42:19 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+console.log('main.js loaded!');
+
+$(document).ready(function() {
+  console.log('document loaded!');
+});
+
+
+  
+  $template = document.querySelector('#template');
+  $anchor = document.querySelector('#anchor');
+  let template = _.template($template.innerHTML);
+  $anchor.innerHTML = template({content: fancyData})
+  
